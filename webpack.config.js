1461507191473acf08a90663f4ffc8c930f03ca4
Luis Rueda <lurueda@cisco.com>
2022-03-25 12:27:33 -0500

    Fix dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
+const path = require('path');
+
 module.exports = {
-  entry: './src/index.ts',
+  entry: './src/compose.ts',
   output: {
-    filename: './dist/bundle.js',
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  devtool: 'source-map',
+  devtool: 'inline-source-map',
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+    fallback: {
+      constants: false,
+      crypto: false,
+      fs: false,
+      http: false,
+      https: false,
+      os: false,
+      path: false,
+      stream: false,
+      util: false,
+      zlib: false,
+    },
   },
   module: {
     rules: [
